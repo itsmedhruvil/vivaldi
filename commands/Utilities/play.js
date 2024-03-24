4dce5800af36b98daa50351089f7918f186dff81
dhruvilshah <200305124903@paruluniversity.ac.in>
2024-03-24 13:05:14 +0530

    new files

--- commands/Utilities/play.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-
-export const data = new SlashCommandBuilder()
-  .setName('ping')
-  .setDescription('replies with pong! ');
-
-export async function execute(interaction) {
-  await interaction.reply('Pong!');
-}
